95a90db2d2d4cc5574ecd0253cfe4555c8aa4c10
Dieguit0123 <diego_bz30@hotmail.com>
2023-01-28 12:10:38 -0300

    finishing

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -163,7 +163,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  let nuevaString = str + "!"
+  let nuevaString = str + "!";
   return nuevaString;
 }
 
@@ -179,14 +179,13 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  let nuevaString = "Hola "+ "Martin" + "!";
-  return nuevaString;
+  return "Hola " + nombre + "!"
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  let area = alto*ancho;
+  let area = alto * ancho;
   return area;
 }
 
@@ -219,8 +218,13 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
- if(letra.length !== 1 || letra !== "a" && letra !== "e" && letra !== "i" && letra !== "o" && letra !== "u") return "Dato Incorrecto"
- return "Es Vocal";
+ if(letra . longitud > 1){
+  return "Dato incorrecto"
+ } 
+ if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+  return "Es vocal"
+}
+ return "Dato incorrecto";
 }
 
 // No modificar nada debajo de esta línea
